c8c8864eb8da243a38db000ec05dcbff4a44c8be
Brian Wentzloff <brian.wentzloff@gmail.com>
2022-09-04 10:59:34 -0500

    persist store in localStorage

--- src/main.js
+++ src/main.js
@@ -11,10 +11,24 @@ const store = createStore({
     mutations: {
       increment (state) {
         state.count++
+      },
+      initialiseStore(state) {
+        // Check if the ID exists
+        if(localStorage.getItem('store')) {
+            // Replace the state object with the stored item
+            this.replaceState(
+                Object.assign(state, JSON.parse(localStorage.getItem('store')))
+            );
+        }
       }
     }
   })
-  
+store.commit('initialiseStore')
+store.subscribe((mutation, state) => {
+	// Store the state object as a JSON string
+	localStorage.setItem('store', JSON.stringify(state));
+});
+
 
 const app = createApp(App)
 app.use(store)
