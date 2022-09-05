ef094fd3d3c810b253aa42b7a8284423b8c1a9ce
Brian Wentzloff <brian.wentzloff@gmail.com>
2022-09-05 11:38:20 -0500

    createComponent function in contract to handle token URIs and Ids

--- contracts/contracts/TestContract.sol
+++ contracts/contracts/TestContract.sol
@@ -1,12 +1,26 @@
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract TestContract is ERC721 {
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
-        console.log("name", name);
-        console.log("symbol", symbol);
-        console.log("msg.sender", msg.sender); //msg.sender is the address that initially deploys a contract
+contract TestContract is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("TestContract", "TC") {
+        console.log(msg.sender);
+    }
+
+    function createComponent(address creator, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newItemId = _tokenIds.current();
+        _mint(creator, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
     }
 }
--- contracts/test/test.js
+++ contracts/test/test.js
@@ -1,12 +1,43 @@
 const { expect } = require("chai");
 
-describe("TestContract", function () {
+describe("TestContract", async () => {
+  
   it("Should return the right name and symbol", async function () {
     const TestContract = await hre.ethers.getContractFactory("TestContract");
-    const testContract = await TestContract.deploy("TestContract", "TC");
+    const testContract = await TestContract.deploy();
 
     await testContract.deployed();
     expect(await testContract.name()).to.equal("TestContract");
     expect(await testContract.symbol()).to.equal("TC");
+  });
+  0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
+  it("Should mint new NFT when requested", async function () {
+    const TestContract = await hre.ethers.getContractFactory("TestContract");
+    const testContract = await TestContract.deploy();
+
+    await testContract.deployed();
+    const txResponse = await testContract.createComponent('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266','https://game.example/item-id-8u5h2m.png')
+    const txReceipt = await txResponse.wait();
+    const [transferEvent] = txReceipt.events;
+    const tokenId1 = transferEvent.args['tokenId']
+    const txResponse2 = await testContract.createComponent('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266','https://game.example/itemasdf.png')
+    const txReceipt2 = await txResponse2.wait();
+    const [transferEvent2] = txReceipt2.events;
+    const tokenId2 = transferEvent2.args['tokenId']
+
+    const Uri1 = await testContract.tokenURI(tokenId1)
+    console.log(Uri1)
+
+    const Uri2 = await testContract.tokenURI(tokenId2)
+    console.log(Uri2)
+
+
+
+
+
+    /*     console.log(await testContract.createComponent('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266','https://game.example/item-id-8u5h2m.png'))
+    console.log(await testContract.createComponent('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266','https://game.example/item-id-8u5h2m.png'))
+ */    /* expect(await testContract.createComponent()).to.equal("TestContract");
+    expect(await testContract.symbol()).to.equal("TC"); */
   });
 });
